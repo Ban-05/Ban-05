50f5abcc099df2aa29c121e2ed3ec19398c9c028
Ban-05 <igoat149@gmail.com>
2024-10-14 22:37:42 +0000

    Update and rename frontend/main.js to frontend/imageshops/main.js

---
+++ frontend/imageshops/main.js
@@ -0,0 +1,11 @@
+async function registerUser() {
+    // Implémentation du code d'inscription
+}
+
+async function loginUser() {
+    // Implémentation du code de connexion
+}
+
+async function uploadFiles() {
+    // Implémentation du code pour le téléchargement
+}
